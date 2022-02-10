16ec191b3fe1f13d2abea1d80beffc11463aa290
Aleksey <aleksey.doublekill@gmail.comm>
2022-02-10 11:36:35 +0700

    Добавлены заголовки вспомогательных функций для первой лабораторной

--- Files_Operations_Test/Files_Operations_Test.cpp
+++ Files_Operations_Test/Files_Operations_Test.cpp
@@ -1,11 +1,52 @@
 ﻿#include <iostream>
+#include <fstream>
 
-void pudge()
+#include <string>
+
+bool fstreamFileWriteReadTest()
 {
-    std::cout << "pudge\n";
+    std::ofstream writer("test.txt");
+    std::ifstream reader;
+
+    if (!writer.is_open())
+    {
+        std::cerr << "can't open 'test.txt'!" << std::endl;
+        return false;
+    }
+
+    writer << "test";
+    writer.close();
+
+    reader.open("test2.txt");
+    if (!reader.is_open())
+    {
+        std::cerr << "can't open 'test2.txt'!" << std::endl;
+        reader.open("test.txt");
+
+        if (!reader.is_open())
+        {
+            std::cerr << "can't open 'test.txt'!" << std::endl;
+            return false;
+        }
+    }
+
+    std::string fileContent;
+    reader >> fileContent;
+    std::cout << "test.txt content: " << std::endl;
+    std::cout << fileContent << std::endl;
+    reader.close();
+
+
+    return true;
 }
+
+bool createFileWithNumbers(std::string fileName, const int numbersCount, int maxNumberValue);
+
+bool isFileContainsSortedArray(std::string fileName);
+
+
 
 int main()
 {
-    pudge();
+    fstreamFileWriteReadTest();
 }
