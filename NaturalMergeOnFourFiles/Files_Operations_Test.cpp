a3a2737031a320d7b661250bff46e7164303b5ab
Aleksey <aleksey.doublekill@gmail.comm>
2022-02-23 14:02:27 +0700

    Добавлены файлы с функциями работы с файлами

---
+++ NaturalMergeOnFourFiles/Files_Operations_Test.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <random>
+
+#include "Files_Operations.h"
+
+
+bool createFileWithNumbers(std::string fileName, const int numbersCount, const int maxNumberValue)
+{
+    srand(time(NULL));
+    std::ofstream writer(fileName);
+    if (!writer.is_open())
+    {
+        std::cerr << "can't open " << fileName << std::endl;
+        return false;
+    }
+
+    for (int i = 0; i < numbersCount; ++i)
+    {
+        writer << rand() % (maxNumberValue + 1) << ' ';
+    }
+
+    writer.close();
+
+    return true;
+}
+
+bool isFileContainsSortedArray(std::string fileName)
+{
+    std::ifstream reader(fileName);
+
+    if (!reader.is_open())
+    {
+        std::cerr << "can't open " << fileName << std::endl;
+        return false;
+    }
+    
+    int previousNumber;
+    int currentNumber;
+
+    reader >> previousNumber;
+    while (!reader.eof())
+    {
+        reader >> currentNumber;
+        if (currentNumber < previousNumber)
+        {
+            return false;
+        }
+
+        previousNumber = currentNumber;
+    }
+
+    reader.close();
+    return true;
+}
+
+void createFileWithNumbersTest()
+{
+    std::string fileName("test.txt");
+    const int numbersCount = 15;
+    const int maxNumberValue = 25;
+    bool test = createFileWithNumbers(fileName, numbersCount, maxNumberValue);
+    std::cout << test << std::endl;
+}
