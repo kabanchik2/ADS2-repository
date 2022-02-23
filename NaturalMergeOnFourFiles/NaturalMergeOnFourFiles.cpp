62ff259964135e900472a180e4e90d54cea24738
Aleksey <aleksey.doublekill@gmail.comm>
2022-02-23 14:09:55 +0700

    Написана функция разбиения файла

--- NaturalMergeOnFourFiles/NaturalMergeOnFourFiles.cpp
+++ NaturalMergeOnFourFiles/NaturalMergeOnFourFiles.cpp
@@ -1,20 +1,34 @@
-// NaturalMergeOnFourFiles.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+
+#include "Files_Operations.h"
+
+void spliting(std::fstream* A, std::fstream* F, const int p)
+{
+    int x1, x2;
+    int n = 0;
+    *A >> x1;
+
+    while (!A->eof())
+    {
+        F[n] << x1;
+
+        *A >> x2;
+        if (x1 > x2) 
+            n = 1 - n;
+
+        x1 = x2;
+    }
+}
+
+
+
+
+
+
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
