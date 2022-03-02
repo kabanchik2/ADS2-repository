6252761db09b5ece6fd76bc113fe30eaae1ee788
Aleksey <aleksey.doublekill@gmail.comm>
2022-03-02 19:37:03 +0700

    Создан проект двоичных деревьев

---
+++ Trees/BinaryTree.cpp
@@ -0,0 +1,6 @@
+#include "BinaryTree.h"
+
+Node* BinaryTree::getRoot()
+{
+	return m_root;
+}
---
+++ Trees/BinaryTree.h
@@ -0,0 +1,23 @@
+#pragma once
+
+struct Node
+{
+	Node(const int key = 13, Node* leftChild = nullptr, Node* rightChild = nullptr) :
+		key(key),
+		leftChild(leftChild),
+		rightChild(rightChild)
+	{}
+
+	int key = 10;
+	Node* leftChild = nullptr;
+	Node* rightChild = nullptr;
+};
+
+class  BinaryTree
+{
+public:
+	BinaryTree() = default;
+
+private:
+	Node* m_root = nullptr;
+};
