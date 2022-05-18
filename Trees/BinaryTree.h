0e101c7e43dcc526a0752289a126744c9d1bf64c
Aleksey <aleksey.doublekill@gmail.comm>
2022-05-18 15:13:14 +0700

    Некоторые методы убраны под protected

--- Trees/BinaryTree.h
+++ Trees/BinaryTree.h
@@ -38,27 +38,37 @@ public:
 	Node* addNode(Node* subTreeRoot, const int key);
 
 	void printHorizontal();
-	void printHorizontal(Node* subTreeRoot, const int level = 0);
-	void printVertical(Node* subTreeRoot);
 	void printVertical();
 
 	void printLevel(const int level);
-	void printLevel(Node* subTreeRoot, const int level, const int currentLevel = 0);
+	
 
 	Node* node(const int nodeIndex);
-	Node* node(Node *subTreeRoot, int nodeIndex); 
+	
 
 	void copyTree(const Node& other, Node*& destination);
 	void destroy(Node* subTreeRoot);
 	void destroyChildTrees(Node* parentNode);
 
-	int getDepth(const Node* subTreeRoot);
 	int getDepth();
 
-	Node* findParentByKey(Node* subTreeRoot, const int key);
 	Node* findParentByKey(const int key);
 
 	bool deleteNode(Node* nodeToDelete);
+
+
+protected:
+	void printHorizontal(Node* subTreeRoot, const int level = 0);
+	void printVertical(Node* subTreeRoot);
+
+	void printLevel(Node* subTreeRoot, const int level, const int currentLevel = 0);
+
+	Node* node(Node* subTreeRoot, int nodeIndex);
+
+	int getDepth(const Node* subTreeRoot);
+
+	Node* findParentByKey(Node* subTreeRoot, const int key);
+
 
 private:
 	Node* m_root = nullptr;
