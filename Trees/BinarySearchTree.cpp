99ccb1429954803a15cb9af3c06ea4e31aa84b40
Aleksey <aleksey.doublekill@gmail.comm>
2022-06-05 20:59:34 +0700

    Добавлены новые методы:
    добавление узла, получение мин/макс ключа

--- Trees/BinarySearchTree.cpp
+++ Trees/BinarySearchTree.cpp
@@ -10,6 +10,84 @@ BinarySearchTree::BinarySearchTree(const const BinarySearchTree& other) : Binary
 
 BinarySearchTree::~BinarySearchTree()
 {
-	destroy(this->getRoot());
+	destroy(m_root);
 }
 
+bool BinarySearchTree::addNode(const int key)
+{
+	if (m_root == nullptr)
+	{
+		m_root = new Node(key);
+	}
+	else
+	{
+		Node* tmp = m_root;
+		while (tmp->leftChild && key < tmp->getKey() || tmp->rightChild && key >= tmp->getKey())
+		{
+			if (key < tmp->getKey())
+			{
+				tmp = tmp->leftChild;
+			}
+			else
+			{
+				tmp = tmp->rightChild;
+			}
+		}
+
+		if (key < tmp->getKey())
+		{
+			Node* newLeftNode = new Node(key);
+			tmp->leftChild = newLeftNode;
+		}
+		else
+		{
+			Node* newRightNode = new Node(key);
+			tmp->rightChild = newRightNode;
+		}
+	}
+	
+
+	return true;
+}
+
+int BinarySearchTree::getMinimumKey()
+{
+	return getNodeWithMinimumKey(m_root)->getKey();
+}
+
+int BinarySearchTree::getMaximumKey()
+{
+	return getNodeWithMaximumKey(m_root)->getKey();
+}
+
+Node* BinarySearchTree::getNodeWithMinimumKey(Node* subTreeRoot)
+{
+	if (subTreeRoot == nullptr)
+	{
+		return nullptr;
+	}
+
+	Node* tmp = subTreeRoot;
+	while (tmp->leftChild)
+	{
+		tmp = tmp->leftChild;
+	}
+
+	return tmp;
+}
+
+Node* BinarySearchTree::getNodeWithMaximumKey(Node* subTreeRoot)
+{
+	if (subTreeRoot == nullptr)
+	{
+		return nullptr;
+	}
+
+	Node* tmp = subTreeRoot;
+	while (tmp->rightChild)
+	{
+		tmp = tmp->rightChild;
+	}
+
+	return tmp;
+}
--- Trees/BinarySearchTree.h
+++ Trees/BinarySearchTree.h
@@ -4,10 +4,18 @@
 
 class BinarySearchTree : public BinaryTree
 {
+public:
 	BinarySearchTree();
 	BinarySearchTree(const const BinarySearchTree& other);
 	~BinarySearchTree();
 	
-	
+	bool addNode(const int key) override;
+
+	int getMinimumKey() override;
+	int getMaximumKey() override;
+
+protected:
+	Node* getNodeWithMinimumKey(Node* subTreeRoot) override;
+	Node* getNodeWithMaximumKey(Node* subTreeRoot) override;
 };
 
