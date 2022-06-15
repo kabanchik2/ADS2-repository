056e97189ebeecd2c734916979dc016948f5f090
Aleksey <aleksey.doublekill@gmail.comm>
2022-06-15 12:52:28 +0700

    тестовые функции

--- Trees/main.cpp
+++ Trees/main.cpp
@@ -6,6 +6,142 @@
 //#include "BinaryTree.h"
 #include "BinarySearchTree.h"
 
+bool testSearchTree(int size)
+{
+	int failedCase = 0;
+	BinarySearchTree searchTree;
+	std::vector<int> nodesKeys;
+
+
+	for (int i = 0; i < size; ++i) {
+		searchTree.addNode(i);
+		nodesKeys.push_back(i);
+	}
+
+
+	int index, key;
+
+
+	while (nodesKeys.size()) {
+		//	index = rand() % nodesKeys.size();
+		//	nodesKeys.erase(nodesKeys.begin() + index);
+		//	if (!searchTree.deleteKnot(searchTree.getRoot(), index)) {
+		//		failedCase = 1;
+		//		break;
+		//	}
+
+
+			/*if (testSearchTree(searchTree, nodesKeys.size())) {
+				failedCase = 2;
+				break;
+			}*/
+
+
+		if (nodesKeys.size()) {
+			index = rand() % nodesKeys.size();
+			key = nodesKeys[index];
+			nodesKeys.erase(nodesKeys.begin() + index);
+			if (!searchTree.findAndDeleteByKey(key)) {
+				failedCase = 3;
+				break;
+			}
+
+
+			/*if (testSearchTree(searchTree, nodesKeys.size())) {
+				failedCase = 4;
+				break;
+			}*/
+		}
+	}
+
+	if (failedCase || !searchTree.isEmpty()) {
+		if (!failedCase) {
+			failedCase = -1;
+		}
+
+
+		std::cout << size << " " << searchTree.getNumberOfNodes() << std::endl;
+		searchTree.printHorizontal();
+		std::cout << std::endl;
+		std::cout << std::endl;
+
+
+
+
+	}
+	return (failedCase == 0);
+}
+
+bool testSearchTreeAsBinaryTree(int size)
+{
+	int failedCase = 0;
+	BinarySearchTree searchTree;
+	BinaryTree& searchTreeRef = searchTree;
+	std::vector<int> nodesKeys;
+
+
+	for (int i = 0; i < size; ++i) {
+		searchTreeRef.addNode(i);
+		nodesKeys.push_back(i);
+	}
+
+
+	int index, key;
+
+
+	while (nodesKeys.size()) {
+		/*index = rand() % nodesKeys.size();
+		nodesKeys.erase(nodesKeys.begin() + index);
+		if (!searchTreeRef.deleteKnot(searchTreeRef.getRoot(), index)) {
+			failedCase = 1;
+			break;
+		}*/
+
+
+		/*if (testSearchTreeAsBinaryTree(searchTreeRef, nodesKeys.size())) {
+			failedCase = 2;
+			break;
+		}*/
+
+
+		if (nodesKeys.size()) {
+			index = rand() % nodesKeys.size();
+			key = nodesKeys[index];
+			nodesKeys.erase(nodesKeys.begin() + index);
+			if (!searchTreeRef.findAndDeleteByKey(key)) {
+				failedCase = 3;
+				break;
+			}
+
+
+			/*if (testSearchTreeAsBinaryTree(searchTreeRef, nodesKeys.size())) {
+				failedCase = 4;
+				break;
+			}*/
+		}
+	}
+
+
+	if (failedCase || !searchTreeRef.isEmpty()) {
+		if (!failedCase) {
+			failedCase = -1;
+		}
+
+
+		std::cout << size << " " << searchTreeRef.getNumberOfNodes() << std::endl;
+		searchTreeRef.printHorizontal();
+		std::cout << std::endl;
+		std::cout << std::endl;
+
+
+
+
+	}
+	return (failedCase == 0);
+}
+
+
+
 int main()
 {
 	srand(time(0));
@@ -19,7 +155,9 @@ int main()
 		bst.addNode(key);
 	}*/
 
-	bst.addNode(5);
+	//
+
+	/*bst.addNode(5);
 	bst.addNode(2);
 	bst.addNode(8);
 	bst.addNode(1);
@@ -29,33 +167,38 @@ int main()
 	bst.addNode(9);
 	bst.addNode(10);
 
-	bst.printVertical();
+	bst.printVertical();*/
 
-	bst.findAndDeleteByKey(5);
+	//bst.findAndDeleteByKey(5);
 
-	bst.printVertical();
-	
-	//std::cout << bst.getMinimumKey() << ' ' << bst.getMaximumKey() << std::endl;
+	//bst.printVertical();
 
-	/*BinaryTree* tree = &bst;
+	std::cout << std::endl;
+	std::cout << "min: " << bst.getMinimumKey() << 
+				" max: " << bst.getMaximumKey() << std::endl;
 
-	tree->destroy();
+	//BinaryTree* tree = &bst;
 
-	tree->addNode(5);
-	tree->addNode(2);
-	tree->addNode(8);
-	tree->addNode(1);
-	tree->addNode(3);
-	tree->addNode(6);
-	tree->addNode(10);
-	tree->addNode(9);
-	tree->addNode(10);
+	//tree->destroy();
 
-	tree->printVertical();
+	//tree->addNode(5);
+	//tree->addNode(2);
+	//tree->addNode(8);
+	//tree->addNode(1);
+	//tree->addNode(3);
+	//tree->addNode(6);
+	//tree->addNode(10);
+	//tree->addNode(9);
+	//tree->addNode(10);
 
-	tree->findAndDeleteByKey(9);
+	//tree->printVertical();
 
-	tree->printVertical();*/
+	//tree->findAndDeleteByKey(8);
+
+	//tree->printVertical();
+
+	return testSearchTree(150) &&
+	testSearchTreeAsBinaryTree(150);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
