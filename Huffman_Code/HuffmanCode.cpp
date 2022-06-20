43dfc98f2c7b94424187dc35b02a4ef20f370163
Aleksey <aleksey.doublekill@gmail.comm>
2022-06-20 21:53:54 +0700

    HNode конструкторы

--- Huffman_Code/HuffmanCode.cpp
+++ Huffman_Code/HuffmanCode.cpp
@@ -1,1 +1,10 @@
 #include "HuffmanCode.h"
+
+HNode::HNode(std::string key, int frequency, HNode* left, HNode* right)
+{
+	m_key = key;
+	m_frequency = frequency;
+	m_left = left;
+	m_right = right;
+}
+
--- Huffman_Code/HuffmanCode.h
+++ Huffman_Code/HuffmanCode.h
@@ -1,4 +1,23 @@
 #pragma once
+
+#include <string>
+class HNode
+{
+public:
+
+	HNode() = default;
+	HNode(std::string key, int frequency, HNode* left = nullptr, HNode* right = nullptr);
+
+private:
+	int m_frequency;
+	std::string m_key;
+	HNode* m_left;
+	HNode* m_right;
+
+};
+
+
+
 class HuffmanCode
 {
 };
